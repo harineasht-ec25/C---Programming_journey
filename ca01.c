b98f052c267ca8329734f9d5ac273146a74f8f1d
Harineash T <harineash007@gmail.com>
2026-05-23 17:25:15 +0530

    Add files via upload

---
+++ ca01.c
@@ -0,0 +1,15 @@
+// Program to get an alphabet and print its ASCII value
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    // Read an alphabet (stored as int)
+    scanf("%c", &ch);
+
+    // Print ASCII value
+    printf("%d", ch);
+
+    return 0;
+}
