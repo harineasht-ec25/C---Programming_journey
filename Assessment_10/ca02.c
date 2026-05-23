436311da7b4a5cd9d2c4615a68e8681b3a2d00c6
Harineash T <harineash007@gmail.com>
2026-05-23 17:27:14 +0530

    Add files via upload

---
+++ Assessment_10/ca02.c
@@ -0,0 +1,15 @@
+// Program to get a single number and print its ASCII value
+#include <stdio.h>
+
+int main()
+{
+    char num;
+
+    // Read a single digit (as character, stored in int)
+    scanf("%c", &num);
+
+    // Print ASCII value
+    printf("%d", num);
+
+    return 0;
+}
